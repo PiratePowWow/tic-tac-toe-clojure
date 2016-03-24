6c17a231c80c60ae14058fc7fe270c68ea656ab9
PiratePowWow <Thi3v3ry>
2016-03-24 10:29:50 -0400

    8-4 Lecture Tic Tac Toe

--- src/tic_tac_toe_clojure/core.clj
+++ src/tic_tac_toe_clojure/core.clj
@@ -1,5 +1,29 @@
 (ns tic-tac-toe-clojure.core
   (:gen-class))
 
+(def board [[:e :e :e]
+            [:e :e :e]
+            [:e :e :e]])
+
+(defn solve [x]
+  (let [[[a b c]
+         [d e f]
+         [g h i]] x
+        solutions (hash-set
+                    [a b c]
+                    [d e f]
+                    [g h i]
+                    [a d g]
+                    [b e h]
+                    [c f i]
+                    [a e i]
+                    [g e c])]
+    (cond
+      (contains? solutions [:x :x :x]) :x
+      (contains? solutions [:o :o :o]) :o
+      :else nil)))
+    
+
+
 (defn -main []
   (println "Hello, World!"))
